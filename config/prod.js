bb440e273d47071cd8eab65feadbc88dc4e84bdd
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-15 23:23:58 -0700

    work on client side billing

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,11 @@
 //production keys go here
 //do not commit this, dev.js
+// values pulled from Heroku environment vars
 module.exports={
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSectret:process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY //added string to encrypt cookie
+  cookieKey: process.env.COOKIE_KEY, //added string to encrypt cookie
+  stripePublishableKey: process.env.STRIPLE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
---
+++ hey.java
@@ -0,0 +1,5 @@
+Person Ella = new Person(pretty);
+
+if(Ella.Like(me)){
+  fuck.Me(now)
+}
