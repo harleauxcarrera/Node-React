576d6d60eb3123954ecb88296c1cf5dc54234586
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-28 07:36:25 -0700

    editing index.js

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ app.use(passport.session());
                               //attatch 'app' object to external routes file
 require('./routes/authRoutes')(app);//immediatley calls with app object
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
 
 /*Must do following for Google Auth strategy to work:
         go to console.developers.google.com
