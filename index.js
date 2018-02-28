15912cbaad12915440414e52a78cfed844cc3789
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-28 07:49:55 -0700

    fixed react-app stripe key

--- index.js
+++ index.js
@@ -34,18 +34,6 @@ app.use(passport.session());
                               //attatch 'app' object to external routes file
 require('./routes/authRoutes')(app);//immediatley calls with app object
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
-
-  // Express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 
 
