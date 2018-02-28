8cd39ecbdea7739bd333daf791b7d4497e8956f3
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-28 07:47:50 -0700

    fixed react-app stripe key

--- index.js
+++ index.js
@@ -34,6 +34,20 @@ app.use(passport.session());
                               //attatch 'app' object to external routes file
 require('./routes/authRoutes')(app);//immediatley calls with app object
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
 
 /*Must do following for Google Auth strategy to work:
         go to console.developers.google.com
