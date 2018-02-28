3d8654be81c52993c23cde1ec2af7b76d90596a5
Carlos Herrera <harleauxcarrera@Harleauxs-MacBook-Pro.local>
2018-02-27 21:37:53 -0700

    pushing ignored files

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+//do not commit this, dev.js
+module.exports={
+  googleClientID: "939788423599-bjv3gjr9qbsii5bmlh0iivstmk2jqnpc.apps.googleusercontent.com",
+  googleClientSectret:"l7aQif7Nq2lG-3RarKp2BDCo",
+  mongoURI: "mongodb://harleauxcarrera:please313@ds151955.mlab.com:51955/nodereactudemy",
+  cookieKey: "sjhgdfsjhdgfsjdhfgslfasdflkj", //added string to encrypt cookie
+  stripePublishableKey: 'pk_test_BYCElyRhvU4BXYxpvnXFzHzf',
+  stripeSecretKey: 'sk_test_tVmcWAX5YStXw3xA4s29x56W'
+
+};
